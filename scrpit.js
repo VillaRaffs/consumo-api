d5ddf1249f5bce3b4497831ce670851671372f3f
Rafaela Villatore <morango20n1@gmail.com>
2025-08-20 08:49:41 -0300

    Add files via upload

--- scrpit.js
+++ scrpit.js
@@ -6,10 +6,40 @@ async function fetchCharacters(page){
     resultDiv.innerHTML = "<p>Carregando...</p>"
 
     try {
-        const response = await fetch('https://rickandmortyapi.com/api/character')
+        const response = await fetch(`https://rickandmortyapi.com/api/character?page=${page}`)
         const data = await response.json()
-        console.log(data)
+        //console.log(data)
+
+        if(data.error){
+            resultDiv.innerHTML = "<p>Página inválida! Tente outra. (1/42)</p>"
+         return
+        }
+
+        resultDiv.innerHTML = ""
+        data.results.forEach(character =>{
+            const card = document.createElement("div")
+            card.className = "card"
+            card.innerHTML = `
+            <img src="${character.image}" alt="${character.image}">
+            <h3>${character.name}</h3>
+            <p><Strong>Status:</strong>${character.status}</p>
+            <p><Strong>Espécie:</strong>${character.species}</p>
+           `
+            resultDiv.appendChild(card)
+        })
     } catch (error) {
-        
+        //console.log("deu ruim")   
+        resultDiv.innerHTML = "<p> Erro ao buscar personagens</p>"  
     }
 }
+
+searchBtn.addEventListener("click", () => {
+    const page = pageInput.ariaValueMax.value.trim()
+    if(page){
+        fetchCharacters(page)
+    }else{
+        resultDiv.innerHTML = "<p> Digte o número da página</p>"
+    }
+})
+
+fetchCharacters(1)
